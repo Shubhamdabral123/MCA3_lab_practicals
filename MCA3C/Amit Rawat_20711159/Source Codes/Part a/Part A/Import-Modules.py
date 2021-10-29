300f60df3574b2ce93f0cb9b405343fe2816b29c
Amit Rawat <79750803+Amit7777y@users.noreply.github.com>
2021-10-29 09:51:54 +0530

    Add files via upload

---
+++ MCA3C/Amit Rawat_20711159/Source Codes/Part a/Part A/Import-Modules.py
@@ -0,0 +1,9 @@
+# AMIT RAWAT MCA 3 - DEHRADUN Campus STD ID-20711159
+
+from math import *
+
+import math
+print(math.pi)
+
+print(math.sin(math.pi/2))
+print(factorial(7))
---
+++ MCA3C/Amit Rawat_20711159/Source Codes/Part a/Part A/main.py
@@ -0,0 +1,9 @@
+# AMIT RAWAT MCA 3 - DEHRADUN Campus STD ID-20711159
+
+tuple1 = ("AMIT", 22, "Male")
+print(tuple1)
+
+tuple1 = ("GEHU", "GEHU_DEHRADUN", "GEHU_DEEHRADUN")
+tuple2 = (1, 2, 3)
+print(tuple1)
+print(tuple2)
